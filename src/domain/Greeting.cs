c81dbe717c884536d6861221a01095048a06eab4
mo k <mo@mokhan.ca>
2012-02-02 16:55:33 -0700

    start writing Greeting specs.

---
+++ src/domain/Greeting.cs
@@ -0,0 +1,9 @@
+﻿namespace domain
+{
+    public class Greeting
+    {
+      public string Hello(){
+        throw new System.NotImplementedException();
+      }
+    }
+}
---
+++ src/test/GreetingSpecs.cs
@@ -0,0 +1,26 @@
+﻿namespace test
+{
+  using Machine.Specifications;
+  using domain;
+
+    public class GreetingSpecs
+    {
+      Establish context = () =>
+      {
+        sut  = new Greeting();
+      };
+
+      Because of = () =>
+      {
+        result = sut.Hello();
+      };
+
+      It should_say_hello = () =>
+      {
+        result.ShouldEqual("hello");
+      };
+
+      static Greeting sut;
+      static string result;
+    }
+}
