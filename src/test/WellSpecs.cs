071d432d0f399030e38a4543741a3b108f133acb
mo k <mo@mokhan.ca>
2012-04-12 20:47:34 -0600

    calculate net and gross estimated reserves.

--- src/test/WellSpecs.cs
+++ src/test/WellSpecs.cs
@@ -1,105 +1,51 @@
-namespace test
+﻿namespace test
 {
+  using System;
   using System.Collections.Generic;
+  using System.Linq;
   using Machine.Specifications;
   using domain;
 
   public class WellSpecs
   {
-    Establish context = ()=>
+    public class when_estimating_production
     {
-      //sut = new Well();
-    };
-
-    public class when_projecting_production_from_a_new_well
-    {
-      It should_calculate_the_correct_projection =()=>
+      It should_be_able_to_tell_the_estimated_total_production_for_any_month= () =>
       {
-        var well = new Well(new Month(2013, 01));
-        well.apply_split_for<Oil>(25.Percent());
-        well.apply_split_for<Gas>(25.Percent());
-        well.apply_split_for<NGL>(25.Percent());
-        well.apply_split_for<Condensate>(25.Percent());
+        var parkland100Percent = new Oppurtunity();
+        parkland100Percent.WorkingInterest(100m.Percent());
+        var declineCurve = new DeclineCurve();
+        declineCurve.Composition<Gas>(100m.Percent());
+        declineCurve.Add(0, 100.BOED());
+        parkland100Percent.DeclinesUsing(declineCurve);
 
-        var decline = new DeclineCurve();
-        decline.for_month(1,new BOED(100));
-        well.use(decline);
-
-        well.production_for<Oil>(new Month(2013, 01)).ShouldEqual(new BOED(25));
+        var schedule = new DrillSchedule();
+        var jan2013 = new Month(2013, 01);
+        schedule.Include(parkland100Percent.BringOnlineOn(jan2013));
+        schedule.EstimatedGrossProductionFor<All>(jan2013).ShouldEqual(100.BOED());
+        schedule.EstimatedGrossProductionFor<Gas>(jan2013).ShouldEqual(100.BOED());
+        schedule.EstimatedGrossProductionFor<Oil>(jan2013).ShouldEqual(0.BOED());
+        schedule.EstimatedGrossProductionFor<NGL>(jan2013).ShouldEqual(0.BOED());
+        schedule.EstimatedGrossProductionFor<Condensate>(jan2013).ShouldEqual(0.BOED());
       };
-    }
 
-    static readonly Well sut;
-  }
-  public class DeclineCurve{
-    IDictionary<int, BOED> curve;
-    public DeclineCurve(){
-      curve = new Dictionary<int, BOED>();
-    }
-    public void for_month(int month, BOED volume){
-      curve[month] = volume;
-    }
-    //public void each(Action<int, BOED> action){
-    //}
-  }
+      It should_be_able_to_tell_the_estimated_net_total_production_for_any_month = () =>
+      {
+        var parkland75Percent = new Oppurtunity();
+        parkland75Percent.WorkingInterest(75m.Percent());
+        var declineCurve = new DeclineCurve();
+        declineCurve.Composition<Gas>(50m.Percent());
+        declineCurve.Composition<Oil>(50m.Percent());
+        declineCurve.Add(0, 100.BOED());
+        parkland75Percent.DeclinesUsing(declineCurve);
 
-  public class BOED{
-    decimal volume;
-    public BOED(decimal volume){
-      this.volume = volume;
-    }
-    public bool Equals(BOED other){
-      if(ReferenceEquals(null, other)) return false;
-      return other.volume == this.volume;
-    }
-    public override bool Equals(object other){
-      if(ReferenceEquals(null, other)) return false;
-      if(!(other is BOED)) return false;
-      return Equals((BOED)other);
-    }
-    public override string ToString(){
-      return string.Format("{0} BOED", volume);
-    }
-  }
-  public class Well
-  {
-    Month ip_month;
-    IDictionary<ICommodity, Percent> splits;
-    IList<Projection> projections;
-
-    public Well(Month expected_initial_production_month){
-      this.ip_month = expected_initial_production_month;
-      this.splits = new Dictionary<ICommodity, Percent>();
-      this.projections = new List<Projection>();
-    }
-    public void apply_split_for<Product>(Percent percent) where Product : ICommodity, new()
-    {
-      splits.Add(new Product(), percent);
-    }
-    public void use(DeclineCurve curve){
-      //curve.each( (month,total_volume) => {
-          //projections.Add( splits.Select((split)=> split.create_projection(ip_month.add(month), total_volume));
-      //});
-    }
-    public BOED production_for<Commodity>(Month month){
-      return new BOED(25);
-    }
-  }
-  public class Projection{}
-  public class Percent
-  {
-  }
-  public class Month{
-    public Month(int year, int month){}
-  }
-  public interface ICommodity{}
-  public class Oil : ICommodity{}
-  public class Gas : ICommodity{}
-  public class NGL : ICommodity{}
-  public class Condensate : ICommodity{}
-  public static class Conversion{
-    public static Percent Percent(this int percent){
-      return new Percent();
+        var schedule = new DrillSchedule();
+        var jan2013 = new Month(2013, 01);
+        schedule.Include(parkland75Percent.BringOnlineOn(jan2013));
+        schedule.EstimatedNetProductionFor<All>(jan2013).ShouldEqual(75.BOED());
+        schedule.EstimatedNetProductionFor<Gas>(jan2013).ShouldEqual(37.5m.BOED());
+        schedule.EstimatedNetProductionFor<Oil>(jan2013).ShouldEqual(37.5m.BOED());
+      };
     }
   }
 }
